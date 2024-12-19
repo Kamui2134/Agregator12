6edd6b7d80e9103ba9552a174fa1eec88bebe8fa
Шичков Александр <ctorkey@gmail.com>
2024-12-19 18:59:27 +0300

    games, footer, about casino are done

--- assets/js/index.js
+++ assets/js/index.js
@@ -14,3 +14,27 @@ if (document.querySelector('.game-rating__stars')) {
 		}
 	})
 }
+
+// ROAD-MAP
+
+const roadMapLeft = document.querySelector('.road-map__left')
+const roadMapRight = document.querySelector('.road-map__right')
+const roadMapLeftItems = roadMapLeft.querySelectorAll('.road-map__item')
+const roadMapRightItems = roadMapRight.querySelectorAll('.road-map__item')
+const roadMapLine = document.querySelector('.road-map__line')
+roadMapLine.style.height = `${
+	roadMapLeft.scrollHeight +
+	roadMapLeftItems[1].scrollHeight / 2 -
+	roadMapRightItems[1].scrollHeight / 2
+}px`
+
+for (let i = 0; i < roadMapLeftItems.length; i++) {
+	roadMapRightItems[i].style.marginTop = `${
+		roadMapLeftItems[i].scrollHeight / 2
+	}px`
+	const circle = document.createElement('div')
+	circle.className = 'road-map__circle'
+	roadMapLeftItems[i].style.marginBottom = `${
+		roadMapRightItems[i].scrollHeight / 2
+	}px`
+}
